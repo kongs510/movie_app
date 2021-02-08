1a1c182f7cf9ad9447956f6fec67ce3535882195
kongs <yoursitup90@gmail.com>
2021-02-08 16:22:25 +0900

    영화 api  사용해보기 -해당 사이트 접속해서 json파일 형식으로 파일링크를 복사해서 app2.js 파일안에 함수와 컴포넌트를 생성하여 이용한다.

--- src/App2.js
+++ src/App2.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 
 class App2 extends React.Component {
 
@@ -7,11 +8,12 @@ class App2 extends React.Component {
         movies: [],
     };
 
+    getMovies = async () => {
+        const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    }
+    
     componentDidMount(){
-        //영화 데이터 로딩....!
-        setTimeout(() => {
-           this.setState({ isLoading:false}) 
-        }, 6000);
+     this.getMovies();
     }
 
     render() {
