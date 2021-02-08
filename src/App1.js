ef1a08fc4d86eff39293323e4112a2ce645105ca
kongs <yoursitup90@gmail.com>
2021-02-08 15:35:43 +0900

    리액트 영화평점 웹서비스 만들기 1.create-react-app 2.리액트 기초    2-1 컴포넌트    2-2 JSX    2-3  props 3. map() 함수 사용하기   3-1 map을 활용하여 음식 앱 만들기   3-2 음식앱에 prop-types 도입하기 4.state로 숫자 증감기능 구현하기

---
+++ src/App1.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class App1 extends React.Component {
+    constructor(props){
+        super(props);
+        console.log('hello')
+    }
+
+    state = {
+        count : 0,
+    };
+
+    add = () => {
+        // /console.log('add');
+        this.setState(current => ({
+            count : this.state.count +1,
+        }));
+    };
+    minus = () => {
+        // console.log('minus');
+        this.setState({count : this.state.count -1});
+    }
+    componentDidMount(){
+        console.log('Component rendered');
+    }
+    componentDidUpdate(){
+        console.log('I just updated');
+    }
+    componentWillUnmount(){
+        console.log('Goodbye, cruel world')
+    }
+
+    render() {
+        console.log("I'm rendering")
+        return(
+            <div>
+                <h1>The number is : {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        )}
+}
+
+export default App1;
---
+++ src/App2.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class App2 extends React.Component {
+
+    state ={
+        isLoading : true,
+        movies: [],
+    };
+
+    componentDidMount(){
+        //영화 데이터 로딩....!
+        setTimeout(() => {
+           this.setState({ isLoading:false}) 
+        }, 6000);
+    }
+
+    render() {
+        const {isLoading} = this.state;
+        return (
+            <div>
+            {isLoading ? 'Loading...' : 'we are ready'}
+            </div>
+        );
+    }
+}
+
+export default App2;
